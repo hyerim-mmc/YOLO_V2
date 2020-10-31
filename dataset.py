3161e545e875edc47e5235570493fd9d5d704b5c
hyerim-mmc <rim7951@hanyang.ac.kr>
2020-11-01 01:56:38 +0900

    2020-11-01

--- dataset.py
+++ dataset.py
@@ -1,32 +1,224 @@
 import os
-import wget
-import tarfile
+import random
+import torch
+import numpy as np
+import utils
+import xml.etree.ElementTree as Et
+import torch.utils.data.dataset
+import torchvision.transforms.functional as TF
+
+from torchvision import transforms
+from PIL import Image
 
 
-def load_PASCAL_VOC2012():
-    url = 'http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar'
-    filename = 'VOCtrainval_11-May-2012.tar'
-    data_dir = './PASCAL_VOC_2012'
+class Resize:
+    def __init__(self, output_size, scale_factor=1.15):
+        assert isinstance(output_size, int)
+        self.output_size = output_size
+        self.scale_factor = scale_factor
 
-    if not os.path.exists(data_dir):
-        os.mkdir(data_dir)
-        wget.download(url, data_dir)
-    else:
-        if filename in os.listdir(data_dir):
-            pass
+    def __call__(self, data):
+        image, annotation = data
+        width, height = image.size
+
+        if height > width:
+            new_h, new_w = self.output_size * height / width, self.output_size
+        elif height == width:
+            new_h, new_w = height, width
         else:
-            wget.download(url, data_dir)
-    if filename not in os.listdir(data_dir):
-        raise RuntimeError('Tarfile is not found')
+            new_h, new_w = self.output_size, self.output_size * width / height
 
-    if 'VOCdevkit' in os.listdir(data_dir):
-        print("Dataset already exists")
-    else:
-        with tarfile.open(os.path.join(data_dir, filename), 'r') as untar:
-            print("Start extracting tarfile")
-            untar.extractall(data_dir)
-            print("PASCAL_VOC2012 is downloaded")
+        new_h, new_w = int(new_h * self.scale_factor), int(new_w * self.scale_factor)
+        new_image = TF.resize(image, (new_h, new_w), Image.BILINEAR)
+
+        bboxes = annotation["bbox"]
+        for idx in range(len(bboxes)):
+            _bbox = bboxes[idx]
+            _bbox[0] *= new_w / width
+            _bbox[1] *= new_h / height
+            _bbox[2] *= new_w / width
+            _bbox[3] *= new_h / height
+
+        return (new_image, annotation)
 
 
-if __name__ == '__main__':
-    load_PASCAL_VOC2012()
+class RandomCrop:
+    def __init__(self, output_size):
+        self.output_size = output_size
+
+    def __call__(self, data):
+        image, annotation = data
+        width, height = image.size
+        rand_w = random.randint(0, width - self.output_size)
+        rand_h = random.randint(0, height - self.output_size)
+
+        crop_image = TF.crop(image, rand_h, rand_w, self.output_size, self.output_size)
+
+        bboxes = annotation["bbox"]
+        check = torch.ones((len(bboxes)))
+        for idx in range(len(bboxes)):
+            _bbox = bboxes[idx]
+            _bbox[0] -= rand_w
+            _bbox[1] -= rand_h
+            _bbox[2] -= rand_w
+            _bbox[3] -= rand_h
+
+            center_x, center_y = int((_bbox[0] + _bbox[2]) / 2), int((_bbox[1] + _bbox[3]) / 2)
+
+            if _bbox[0] < 0:
+                _bbox[0] = 1
+            if _bbox[1] < 0:
+                _bbox[1] = 1
+            if _bbox[2] > self.output_size:
+                _bbox[2] = self.output_size - 1
+            if _bbox[3] > self.output_size:
+                _bbox[3] = self.output_size - 1
+            if center_x < 0 or center_x > self.output_size:
+                check[idx] = 0
+            if center_y < 0 or center_y > self.output_size:
+                check[idx] = 0
+
+        check = check == 1
+        annotation["bbox"] = bboxes[check]
+        annotation["label"] = annotation["label"][check]
+
+        print("# of bbox : %d\n" % len(annotation["bbox"]))
+
+        return (crop_image, annotation)
+
+
+class VOC_DataLoad(torch.utils.data.Dataset):
+    def __init__(self, output_size=416, train=True):
+        if os.path.exists("./dataset/train.npy") and os.path.exists("./dataset/val.npy"):
+            self.train_set = np.load("./dataset/train.npy")
+            self.test_set = np.load("./dataset/test.npy")
+        else:
+            self.load_dataset()
+
+        self.train = train
+        self.resize_bd = Resize(output_size=output_size)
+        self.crop_bd = RandomCrop(output_size=output_size)
+        self.transform = transforms.Compose(
+            [
+                transforms.ColorJitter(brightness=0.75, hue=0.1, saturation=0.75),
+                transforms.ToTensor(),
+            ]
+        )
+        self.val_transform = transforms.Compose([transforms.ToTensor()])
+
+        self.category = [
+            "aeroplane",
+            "bicycle",
+            "bird",
+            "boat",
+            "bottle",
+            "bus",
+            "car",
+            "cat",
+            "chair",
+            "cow",
+            "diningtable",
+            "dog",
+            "horse",
+            "motorbike",
+            "person",
+            "pottedplant",
+            "sheep",
+            "sofa",
+            "train",
+            "tvmonitor",
+        ]
+
+    def __len__(self):
+        return len(self.data_set)
+
+    def load_dataset(self):
+        train_path = "./PASCAL_VOC_2012/VOCdevkit/VOC2012/ImageSets/Main/train.txt"
+        validate_path = "./PASCAL_VOC_2012/VOCdevkit/VOC2012/ImageSets/Main/val.txt"
+
+        self.train_files = np.loadtxt(train_path, delimiter="\n", dtype="str")
+        self.validate_files = np.loadtxt(validate_path, delimiter="\n", dtype="str")
+        self.data_set = np.concatenate((self.train_files, self.validate_files))
+
+        random.shuffle(self.data_set)
+        split = int(np.round(0.2 * len(self.data_set)))
+
+        self.train_set, self.test_set = self.data_set[split:], self.data_set[:split]
+
+        np.save("dataset/train.npy", self.train_set)
+        np.save("dataset/test.npy", self.test_set)
+
+    def __getitem__(self, idx):
+        """
+        :return: { 'image' : image, 
+                   'annotation' : {
+                                'label' : [label_index]
+                                'bbox' : [[xmin, ymin, xmax, ymax], [...]]
+                                }
+                }
+        """
+        if self.train:
+            self.image_path = "./Pascal_VOC_2012/VOCdevkit/VOC2012/JPEGImages/" + "{0}.jpg".format(
+                self.train_set[idx]
+            )
+            self.xml_path = "./Pascal_VOC_2012/VOCdevkit/VOC2012/Annotations/" + "{0}.xml".format(
+                self.train_set[idx]
+            )
+
+        else:
+            self.image_path = "./Pascal_VOC_2012/VOCdevkit/VOC2012/JPEGImages/" + "{0}.jpg".format(
+                self.test_set[idx]
+            )
+            self.xml_path = "./Pascal_VOC_2012/VOCdevkit/VOC2012/Annotations/" + "{0}.xml".format(
+                self.test_set[idx]
+            )
+
+        img_name = self.image_path
+        xml_name = self.xml_path
+
+        image = Image.open(img_name)
+        xml = open(xml_name, "r")
+        tree = Et.parse(xml)
+        root = tree.getroot()
+
+        objects = root.findall("object")
+        annotation = {}
+        labels = []
+        bboxes = []
+
+        for _object in objects:
+            name = _object.find("name").text
+            label = self.category.index(name)
+            labels.append(label)
+
+            bndbox = _object.find("bndbox")
+            xmin = float(bndbox.find("xmin").text)
+            ymin = float(bndbox.find("ymin").text)
+            xmax = float(bndbox.find("xmax").text)
+            ymax = float(bndbox.find("ymax").text)
+
+            bboxes.append([xmin, ymin, xmax, ymax])
+
+        annotation["label"] = torch.tensor(labels).float()
+        annotation["bbox"] = torch.tensor(bboxes).float()
+
+        if self.train:
+            image, annotation = self.resize_bd((image, annotation))
+            image, annotation = self.crop_bd((image, annotation))
+            image = self.transform(image)
+        else:
+            # need to check
+            image, annotation = self.resize_bd((image, annotation))
+            image, annotation = self.crop_bd((image, annotation))
+            image = self.val_transform(image)
+
+        return {"image": image, "annotation": annotation}
+
+
+if __name__ == "__main__":
+    VOC_dataset = VOC_DataLoad()
+
+    for idx in range(VOC_dataset.__len__()):
+        print("#%d image" % idx)
+        utils.show_image(VOC_dataset.__getitem__(idx))
+    # print(VOC_dataset.__getitem__(idx))
--- utils.py
+++ utils.py
@@ -1,39 +1,20 @@
+import torch
+import numpy as np
 from PIL import Image
 from PIL import ImageDraw
 from torchvision import transforms
-import numpy as np
-
-
-def show_image_raw(data):
-    img_file, annotation = data["image"], data["annotation"]
-    draw = ImageDraw.Draw(img_file)
-
-    for idx in range(len(annotation)):
-        name = annotation[idx][0]
-        xmin = annotation[idx][1]
-        ymin = annotation[idx][2]
-        xmax = annotation[idx][3]
-        ymax = annotation[idx][4]
-
-        draw.rectangle(((xmin, ymin), (xmax, ymax)), outline="red")
-        draw.text((xmin, ymin), name)
-
-    img_file.show()
 
 
 def show_image(data):
-    img_file, annotation = data["image"], data["annotation"]
-    img_file = transforms.ToPILImage()(img_file)
-    annotation = annotation.tolist()
-    draw = ImageDraw.Draw(img_file)
+    image, annotation = data["image"], data["annotation"]
 
-    for idx in range(len(annotation)):
-        xmin = annotation[idx][0]
-        ymin = annotation[idx][1]
-        xmax = annotation[idx][2]
-        ymax = annotation[idx][3]
+    if torch.is_tensor(image):
+        image = transforms.ToPILImage()(image)
+    draw = ImageDraw.Draw(image)
+    bboxes = annotation["bbox"]
 
-        draw.rectangle(((xmin, ymin), (xmax, ymax)), outline="red", width=2)
-        # draw.text((xmin, ymin), name)
+    for bbox in bboxes:
+        draw.rectangle(((bbox[0], bbox[1]), (bbox[2], bbox[3])), outline="red", width=2)
+        # draw.text((bbox[0], bbpx[1]), name)
 
-    img_file.show()
+    image.show()
