190608c2f69c293d5b1fac071a0d02bf2e23f79d
hyerim-mmc <rim7951@hanyang.ac.kr>
2020-11-11 21:38:12 +0900

    2020-11-11

--- network.py
+++ network.py
@@ -85,7 +85,7 @@ class Pretrain_model:
         device="cpu",
         weight_decay=0.0005,
         momentum=0.9,
-        division=2,
+        division=2,7
         burn_in=1000,
         load_path=None,
     ):
